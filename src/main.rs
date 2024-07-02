99517835663183f3554760ede41a7e755dc85211
Wakingonthesky <2024059451@qq.com>
2024-07-02 14:36:00 +0800

    fix: change the ip

--- src/main.rs
+++ src/main.rs
@@ -107,7 +107,7 @@ async fn main() -> std::io::Result<()> {
             .service(login_phone)
             .route("/hey", web::get().to(manual_hello))
     })
-    .bind(("127.0.0.1", 8095))?
+    .bind(("0.0.0.0", 8095))?
     .run()
     .await
 }
